6a555c90d1c91b12e3e39f65b36cc033f528bea3
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-08-28 00:05:29 +0100

    update

--- components/preview/Slider.js
+++ components/preview/Slider.js
@@ -51,7 +51,7 @@ function Slider({ project }) {
                   opacity: 0,
                 }}
               >
-                <Image src={second} layout="fill" alt="screenshot" priority />;
+                <Image src={second} layout="fill" alt="screenshot" />;
               </div>
 
               <div
@@ -61,7 +61,7 @@ function Slider({ project }) {
                   opacity: 0,
                 }}
               >
-                <Image src={third} layout="fill" alt="screenshot" priority />;
+                <Image src={third} layout="fill" alt="screenshot" />;
               </div>
             </div>
           );
