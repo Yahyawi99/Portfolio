f26a6291bd12b84dcaf17972ce5d193ddfb4a30e
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-08-27 17:26:47 +0100

    update

--- pages/api/single-project/[id].js
+++ pages/api/single-project/[id].js
@@ -3,21 +3,23 @@ import { StatusCodes } from "http-status-codes";
 import connectMongo from "../../../utils/connectDB";
 import Projects from "../../../models/Projects";
 
-import AllProjects from "../../../data.json";
+// import AllProjects from "../../../data.json";
 
 async function getSingleProject(req, res) {
   try {
     const { id } = req.query;
 
-    // await connectMongo();
+    await connectMongo();
 
-    // let project = await Projects.find();
+    let project = await Projects.find();
 
-    // project = project.filter((e) => e._id.toHexString() === id);
+    project = project.filter((e) => e._id.toHexString() === id);
 
-    res
-      .status(StatusCodes.OK)
-      .json({ project: [{ ...AllProjects[0] }], success: true });
+    // res
+    //   .status(StatusCodes.OK)
+    //   .json({ project: [{ ...AllProjects[0] }], success: true });
+
+    res.status(StatusCodes.OK).json({ project, success: true });
   } catch (error) {
     console.log(error);
     res.send();
