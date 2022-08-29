9a4ae2393a2ce6fff554d65d14e492139d01345e
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-08-29 21:54:43 +0100

    update

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import axios from "axios";
+import { useRouter } from "next/router";
 // components
 import Description from "../../components/about/Description";
 import Technologies from "../../components/about/Technologies";
@@ -7,6 +8,12 @@ import Technologies from "../../components/about/Technologies";
 import styles from "../../styles/about/about.module.css";
 
 function About(props) {
+  const { isFallback } = useRouter();
+
+  if (isFallback) {
+    return <h1>Fallback</h1>;
+  }
+
   return (
     <section className={styles.container}>
       <div className={styles.title}>
@@ -27,7 +34,7 @@ function About(props) {
 export const getStaticProps = async () => {
   const response = await axios(`${process.env.BASE_URL}/api/skills`);
 
-  return { props: response.data, fallback: false };
+  return { props: response.data };
 };
 
 export default About;
