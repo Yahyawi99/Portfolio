dfb54e1091b1f89bf2a59689e8e9413bed5cfc95
Yassin Yahyawi <96199527+Yahyawi99@users.noreply.github.com>
2022-08-30 13:54:33 +0100

    update

--- pages/challenges/index.js
+++ pages/challenges/index.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import axios from "axios";
+import { useGlobal } from "../../context";
+import Link from "next/link";
+import Image from "next/image";
 // css
 import styles from "../../styles/projects/projects.module.css";
 
-function Challenges() {
+function Challenges(props) {
+  const { projectHoverEffect } = useGlobal();
+  const { AllChallenges } = props;
+  const ProjectsRef = useRef(null);
+
+  useEffect(() => {
+    projectHoverEffect(ProjectsRef.current);
+  });
+
   return (
     <section className={styles.container}>
       <div>
@@ -11,6 +23,27 @@ function Challenges() {
         <h2>
           <span>FUN</span> PROJECTS
         </h2>
+      </div>
+
+      <div ref={ProjectsRef} className={styles.projectsContainer}>
+        {AllChallenges.length &&
+          AllChallenges.map((e, i) => {
+            const { _id, name, images } = e;
+
+            return (
+              <Link key={_id} href={`/preview/${_id}`}>
+                <div>
+                  <Image src={images.preview} layout="fill" priority />
+
+                  <div className={styles.layer}>
+                    <img src={images.logo} alt="logo" />
+
+                    <h2>{name}</h2>
+                  </div>
+                </div>
+              </Link>
+            );
+          })}
       </div>
     </section>
   );
