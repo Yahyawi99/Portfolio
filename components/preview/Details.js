5f123a690fc7e44065ac256d3eef596ab653915a
Yahyawi99 <yassinyahyawi26@gmail.com>
2025-06-27 12:27:58 +0100

    fixed preview page errors

--- components/preview/Details.js
+++ components/preview/Details.js
@@ -31,25 +31,23 @@ function Details({ data }) {
 
               <span>
                 <p>Code :</p>
-                <Link href={github}>
-                  <a target="_blank">
-                    <p>github</p>
+                <Link href={github} target="_blank">
+                  <p>github</p>
 
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="24"
-                      height="24"
-                      viewBox="0 0 24 24"
-                    >
-                      <path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z" />
-                    </svg>
-                  </a>
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="24"
+                    height="24"
+                    viewBox="0 0 24 24"
+                  >
+                    <path d="M21 13v10h-21v-19h12v2h-10v15h17v-8h2zm3-12h-10.988l4.035 4-6.977 7.07 2.828 2.828 6.977-7.07 4.125 4.172v-11z" />
+                  </svg>
                 </Link>
               </span>
             </div>
 
-            <Link href={live}>
-              <a target="_blank">VISIT</a>
+            <Link href={live} target="_blank">
+              <span>VISIT</span>
             </Link>
           </div>
         );
